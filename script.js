09c1b5a05455cf7be3564126db15ebebd96a927c
Apollo Maria <apollomscarvalho@gmail.com>
2026-06-15 18:08:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ const swiper = new Swiper(".mySwiper", {
 
   loop: true,
 });
+
+const missaoSwiper = new Swiper(".missaoSwiper", {
+    loop: true,
+
+    effect: "fade",
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    
+    autoplay:{
+        delay:5000,
+    }
+});
